edbdbae4547f20c0a012349523f43fa7ebbcbecf
Matheus Rocha Sandim <mrochasandim75@gmail.com>
2022-10-19 19:27:07 -0400

    feat: comecando vue3

---
+++ src/views/HelloWorld.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+    name: 'helloWorld',
+    setup() {
+        
+    },
+}
+</script>
+
+<template>
+    <div>
+        <p class="saudacao">
+            Hello World
+        </p>
+    </div>
+</template>
+
+<style scoped>
+    .saudacao {
+        font-family: sans-serif;
+        font-size: 20px;
+        color: blue;
+    }
+</style>
