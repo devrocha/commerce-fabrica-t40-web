73dd5e001a8be3b09f31d1ecb9d1217796ad093b
Thiesa <thiesa.c@gmail.com>
2022-11-01 08:33:19 -0400

    feat: implementacao do carrinho

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,19 +1,22 @@
 <template>
-	<div class="container">
-		<h1>Quem tem estilo próprio está sempre na moda</h1>
-		<button class="btn">
-			<span class="textButton">Novidades</span>
-		</button>
-		<button class="btn">
-			<span class="textButton">Coleção</span>
-		</button>
-		<button class="btn">
-			<span class="textButton">Favoritos</span>
-		</button>
-		<button class="btn">
-			<span class="textButton">Meus pedidos</span>
-		</button>
-	</div>
+  <div class="container">
+    <h1>Quem tem estilo próprio está sempre na moda</h1>
+    <button class="btn">
+      <span class="textButton">Novidades</span>
+    </button>
+    <button class="btn">
+      <span class="textButton">Coleção</span>
+    </button>
+    <button class="btn">
+      <span class="textButton">Favoritos</span>
+    </button>
+    <button class="btn">
+      <span class="textButton">Meus pedidos</span>
+    </button>
+  </div>
+  <div>
+    <h1>Novos Produtos</h1>
+  </div>
 </template>
 
 <script>
