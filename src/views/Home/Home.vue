7d3934ca75bd8156f7c5761a7cb0a054b8933a8b
Thiesa <thiesa.c@gmail.com>
2022-11-04 13:02:14 -0400

    feat:adicionando produtos

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -16,9 +16,83 @@
       </button>
     </div>
   </div>
-  <div>
-    <h1>Novos Produtos</h1>
-  </div>
+  <section class="product">
+    <h2 class="product-category">Mais Vendidos</h2>
+    <div class="product-container">
+      <div class="product-card">
+        <div class="product-image">
+          <span class="discount-tag">50% off</span>
+          <img
+            src="https://img.abercrombie.com/is/image/anf/KIC_144-2069-1446-900_prod1?policy=product-medium&wid=350&hei=438"
+            class="product-thumb"
+          />
+          <button class="card-btn">adicionar aos favoritos</button>
+        </div>
+        <div class="product-info">
+          <h2 class="product-brand">brand</h2>
+          <p class="product-short-desc">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit
+          </p>
+          <span class="price">R$100</span
+          ><span class="actual-price">R%200</span>
+        </div>
+      </div>
+      <div class="product-card">
+        <div class="product-image">
+          <span class="discount-tag">50% off</span>
+          <img
+            src="https://img.abercrombie.com/is/image/anf/KIC_144-2069-1446-900_prod1?policy=product-medium&wid=350&hei=438"
+            class="product-thumb"
+          />
+          <button class="card-btn">adicionar aos favoritos</button>
+        </div>
+        <div class="product-info">
+          <h2 class="product-brand">brand</h2>
+          <p class="product-short-desc">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit
+          </p>
+          <span class="price">R$100</span
+          ><span class="actual-price">R%200</span>
+        </div>
+      </div>
+      <div class="product-card">
+        <div class="product-image">
+          <span class="discount-tag">50% off</span>
+          <img
+            src="https://img.abercrombie.com/is/image/anf/KIC_144-2069-1446-900_prod1?policy=product-medium&wid=350&hei=438"
+            class="product-thumb"
+          />
+          <button class="card-btn">adicionar aos favoritos</button>
+        </div>
+        <div class="product-info">
+          <h2 class="product-brand">brand</h2>
+          <p class="product-short-desc">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit
+          </p>
+          <span class="price">R$100</span
+          ><span class="actual-price">R%200</span>
+        </div>
+      </div>
+      <div class="product-card">
+        <div class="product-image">
+          <span class="discount-tag">50% off</span>
+          <img
+            src="https://img.abercrombie.com/is/image/anf/KIC_144-2069-1446-900_prod1?policy=product-medium&wid=350&hei=438"
+            class="product-thumb"
+          />
+          <button class="card-btn">adicionar aos favoritos</button>
+        </div>
+        <div class="product-info">
+          <h2 class="product-brand">brand</h2>
+          <p class="product-short-desc">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit
+          </p>
+          <span class="price">R$100</span
+          ><span class="actual-price">R%200</span>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
