6fff38091a011b715e2abf06d1bfce488872a21a
Poliane Oliveira <polianeh@gmail.com>
2022-11-03 21:31:25 -0400

    feat: criacao rota para botoes do footer

---
+++ src/components/Footer/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+      <div class="footer-buttons">
+            <p>Fale Conosco</p>
+            <router-link to="/email"><button><img src="@/assets/images/o-email.png"/></button></router-link>
+            <router-link to="/whatsapp"><button><img src="@/assets/images/whatsapp.png"/></button></router-link>
+            <p>Redes Sociais</p>
+            <router-link to="/instagram"><button><img src="@/assets/images/instagram.png"/></button></router-link>
+            <router-link to="/facebook"><button><img src="@/assets/images/facebook.png"/></button></router-link> 
+        </div>
+</template>
+<script>
+    export default {
+        
+    }
+</script>
+<style lang="scss" scoped src="./style.scss" />
---
+++ src/views/Instagram/Instagram.vue
@@ -0,0 +1,9 @@
+<template>
+	<h1>Teste instagram</h1>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style src="./style.scss" scoped lang="scss" />
