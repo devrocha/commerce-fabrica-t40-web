1d1d861b7c09929e4a61e109cbc883c29774beea
Poliane Oliveira <polianeh@gmail.com>
2022-11-07 19:56:33 -0400

    feat: component button & fix: font

---
+++ src/components/Button/Button.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <button :class="type=='outline' ? 'btn' : 'card-btn'">
+      <span class="textButton">{{ label }}</span>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    label: String,
+    type: String,
+  }
+};
+</script>
+<style lang="scss" scoped src="./style.scss" />
--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,102 +1,105 @@
 <template>
-  <div class="container">
-    <h1>Quem tem estilo próprio está sempre na moda</h1>
-    <div class="container-btn">
-      <button class="btn">
-        <span class="textButton">Novidades</span>
-      </button>
-      <button class="btn">
-        <span class="textButton">Coleção</span>
-      </button>
-      <button class="btn">
-        <span class="textButton">Favoritos</span>
-      </button>
-      <button class="btn">
-        <span class="textButton">Meus pedidos</span>
-      </button>
+  <div>
+    <div class="container">
+      <h1>Quem tem estilo próprio está sempre na moda</h1>
+      <div class="container-btn">
+        <Button label="Novidades" type="outline"/>
+
+        <Button label="Coleção" type="outline"/>
+
+        <Button label="Favoritos" type="outline"/>
+
+        <Button label="Outros" type="outline"/>
+      </div>
     </div>
+    <section class="product">
+      <h2 class="product-category">Mais Vendidos</h2>
+      <div class="product-container">
+        <div class="product-card">
+          <div class="product-image">
+            <span class="discount-tag">50% off</span>
+            <img
+              src="https://img.abercrombie.com/is/image/anf/KIC_144-2069-1446-900_prod1?policy=product-medium&wid=350&hei=438"
+              class="product-thumb"
+            />
+            <Button label="Adicionar aos favoritos"/>
+            <!-- <button class="card-btn">adicionar aos favoritos</button> -->
+          </div>
+          <div class="product-info">
+            <h2 class="product-brand">brand</h2>
+            <p class="product-short-desc">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit
+            </p>
+            <span class="price">R$100</span
+            ><span class="actual-price">R%200</span>
+          </div>
+        </div>
+        <div class="product-card">
+          <div class="product-image">
+            <span class="discount-tag">50% off</span>
+            <img
+              src="https://img.abercrombie.com/is/image/anf/KIC_144-2069-1446-900_prod1?policy=product-medium&wid=350&hei=438"
+              class="product-thumb"
+            />
+            <Button label="Adicionar aos favoritos"/>
+          </div>
+          <div class="product-info">
+            <h2 class="product-brand">brand</h2>
+            <p class="product-short-desc">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit
+            </p>
+            <span class="price">R$100</span
+            ><span class="actual-price">R%200</span>
+          </div>
+        </div>
+        <div class="product-card">
+          <div class="product-image">
+            <span class="discount-tag">50% off</span>
+            <img
+              src="https://img.abercrombie.com/is/image/anf/KIC_144-2069-1446-900_prod1?policy=product-medium&wid=350&hei=438"
+              class="product-thumb"
+            />
+            <Button label="Adicionar aos favoritos"/>
+          </div>
+          <div class="product-info">
+            <h2 class="product-brand">brand</h2>
+            <p class="product-short-desc">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit
+            </p>
+            <span class="price">R$100</span
+            ><span class="actual-price">R%200</span>
+          </div>
+        </div>
+        <div class="product-card">
+          <div class="product-image">
+            <span class="discount-tag">50% off</span>
+            <img
+              src="https://img.abercrombie.com/is/image/anf/KIC_144-2069-1446-900_prod1?policy=product-medium&wid=350&hei=438"
+              class="product-thumb"
+            />
+            <Button label="Adicionar aos favoritos"/>
+          </div>
+          <div class="product-info">
+            <h2 class="product-brand">brand</h2>
+            <p class="product-short-desc">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit
+            </p>
+            <span class="price">R$100</span
+            ><span class="actual-price">R%200</span>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
-  <section class="product">
-    <h2 class="product-category">Mais Vendidos</h2>
-    <div class="product-container">
-      <div class="product-card">
-        <div class="product-image">
-          <span class="discount-tag">50% off</span>
-          <img
-            src="https://img.abercrombie.com/is/image/anf/KIC_144-2069-1446-900_prod1?policy=product-medium&wid=350&hei=438"
-            class="product-thumb"
-          />
-          <button class="card-btn">adicionar aos favoritos</button>
-        </div>
-        <div class="product-info">
-          <h2 class="product-brand">brand</h2>
-          <p class="product-short-desc">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit
-          </p>
-          <span class="price">R$100</span
-          ><span class="actual-price">R%200</span>
-        </div>
-      </div>
-      <div class="product-card">
-        <div class="product-image">
-          <span class="discount-tag">50% off</span>
-          <img
-            src="https://img.abercrombie.com/is/image/anf/KIC_144-2069-1446-900_prod1?policy=product-medium&wid=350&hei=438"
-            class="product-thumb"
-          />
-          <button class="card-btn">adicionar aos favoritos</button>
-        </div>
-        <div class="product-info">
-          <h2 class="product-brand">brand</h2>
-          <p class="product-short-desc">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit
-          </p>
-          <span class="price">R$100</span
-          ><span class="actual-price">R%200</span>
-        </div>
-      </div>
-      <div class="product-card">
-        <div class="product-image">
-          <span class="discount-tag">50% off</span>
-          <img
-            src="https://img.abercrombie.com/is/image/anf/KIC_144-2069-1446-900_prod1?policy=product-medium&wid=350&hei=438"
-            class="product-thumb"
-          />
-          <button class="card-btn">adicionar aos favoritos</button>
-        </div>
-        <div class="product-info">
-          <h2 class="product-brand">brand</h2>
-          <p class="product-short-desc">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit
-          </p>
-          <span class="price">R$100</span
-          ><span class="actual-price">R%200</span>
-        </div>
-      </div>
-      <div class="product-card">
-        <div class="product-image">
-          <span class="discount-tag">50% off</span>
-          <img
-            src="https://img.abercrombie.com/is/image/anf/KIC_144-2069-1446-900_prod1?policy=product-medium&wid=350&hei=438"
-            class="product-thumb"
-          />
-          <button class="card-btn">adicionar aos favoritos</button>
-        </div>
-        <div class="product-info">
-          <h2 class="product-brand">brand</h2>
-          <p class="product-short-desc">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit
-          </p>
-          <span class="price">R$100</span
-          ><span class="actual-price">R%200</span>
-        </div>
-      </div>
-    </div>
-  </section>
 </template>
 
 <script>
-export default {};
+import Button from "@/components/Button/Button.vue"
+export default {
+  components:{
+    Button,
+  }
+};
 </script>
 
 <style src="./style.scss" scoped lang="scss" />
