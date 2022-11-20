4c7902c1538aedde905b76dd1baf8026bf067786
Thiesa <thiesa.c@gmail.com>
2022-11-20 11:01:58 -0400

    fix:header e corpo da home

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -3,48 +3,51 @@ export default {};
 </script>
 
 <template>
-  <v-app>
-    <v-toolbar color="#d0bdf4a3" elevation="4">
-      <v-toolbar-title><img src="@/assets/images/logo.png" /></v-toolbar-title>
+  <!-- <v-app-bar> -->
+  <v-toolbar color="#d0bdf4a3" elevation="4">
+    <v-toolbar-title><img src="@/assets/images/logo.png" /></v-toolbar-title>
 
-      <v-spacer>
-        <router-link to="/" style="text-decoration: none; color: inherit"
-          ><v-button>Home</v-button></router-link
-        >
-        <router-link
-          to="/feminino"
-          style="text-decoration: none; color: inherit"
-          ><v-button>Feminino</v-button></router-link
-        >
-        <router-link
-          to="/masculino"
-          style="text-decoration: none; color: inherit"
-        >
-          <v-button>Masculino</v-button></router-link
-        >
-        <router-link
-          to="/infantil"
-          style="text-decoration: none; color: inherit"
-          ><v-button>Infantil</v-button></router-link
-        >
-      </v-spacer>
-      <input type="text" v-model="input" placeholder="Buscar peça" />
-      <v-col class="ma-2">
-        <v-row justify="end">
-          <v-btn
-            ><img
-              src="@/assets/images/shopping-bag.png"
-              width="30"
-              height="30"
-            />
-          </v-btn>
-          <v-btn
-            ><img src="@/assets/images/user.png" width="30" height="30" />
-          </v-btn>
-        </v-row>
-      </v-col>
-    </v-toolbar>
-  </v-app>
+    <v-spacer>
+      <router-link
+        to="/"
+        style="text-decoration: none; color: inherit"
+        class="mx-6"
+        ><v-button>Home</v-button></router-link
+      >
+      <router-link
+        to="/feminino"
+        style="text-decoration: none; color: inherit"
+        class="mx-6"
+        ><v-button>Feminino</v-button></router-link
+      >
+      <router-link
+        to="/masculino"
+        style="text-decoration: none; color: inherit"
+        class="mx-6"
+      >
+        <v-button>Masculino</v-button></router-link
+      >
+      <router-link
+        to="/infantil"
+        style="text-decoration: none; color: inherit"
+        class="mx-6"
+        ><v-button>Infantil</v-button></router-link
+      >
+    </v-spacer>
+    <input type="text" v-model="input" placeholder="Buscar peça" />
+    <v-col class="ma-2">
+      <v-row justify="end">
+        <v-btn
+          ><img src="@/assets/images/shopping-bag.png" width="30" height="30" />
+        </v-btn>
+        <v-btn
+          ><img src="@/assets/images/user.png" width="30" height="30" />
+        </v-btn>
+      </v-row>
+    </v-col>
+  </v-toolbar>
+  <!-- </v-app> -->
+
   <!-- <div class="container"> -->
   <!-- logo da loja -->
   <!-- <div class="header-logo">
