ed1ae2290829bd76ea114bb4880b6a37c24276b7
Poliane Oliveira <polianeh@gmail.com>
2022-11-22 11:17:48 -0400

    fix: price masculino

--- src/views/Masculino/Masculino.vue
+++ src/views/Masculino/Masculino.vue
@@ -60,7 +60,6 @@ export default {
       <v-row align="center" class="mx-0">
         <v-card-title>{{ peca.name }}</v-card-title>
         <v-card-subtitle>{{ peca.description }}</v-card-subtitle>
-        <div class="my-4 text-subtitle-1">R$ {{ conversion(peca.price) }}</div>
         <v-row>
           <v-col col="6">
             <div class="my-4 text-subtitle-1">
