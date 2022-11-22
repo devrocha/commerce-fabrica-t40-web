6baeb7f4ce292dbc385f4367429d88e46ff98f7d
Rocha <mrochasandim75@gmail.com>
2022-11-21 21:25:58 -0300

    fix: imports

--- src/main.js
+++ src/main.js
@@ -4,9 +4,6 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 
-import "@/assets/styles/index.scss";
-("material-design-icons-iconfont/dist/material-design-icons.css");
-
 import "vuetify/styles";
 import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
