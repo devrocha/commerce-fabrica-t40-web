e81c23b6f0bc81de5c58db639a22c918a4b94f29
Thiesa <thiesa.c@gmail.com>
2022-11-27 09:48:25 -0400

    fix:footer position absolute

--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -3,48 +3,34 @@ export default {};
 </script>
 
 <template>
-  <v-toolbar color="#46AFA5">
-    <v-row align="center">
-          <v-col cols="2">
-           <p>Fale Conosco</p>
-            <router-link to="/email"
-              ><v-btn>
-                <img
-                  src="@/assets/images/o-email.png"
-                  width="30"
-                  height="30" /></v-btn
-            ></router-link>
-            <router-link to="/whatsapp"
-              ><v-btn>
-                <img
-                  src="@/assets/images/whatsapp.png"
-                  width="30"
-                  height="30" /></v-btn
-            ></router-link>
-              </v-col>
-     
-      <v-col cols="8" align="center">
-        <img src="@/assets/images/c.png" width="20" height="20" />
-        <span>2022 T40 Bra, Inc. All Rights reserved</span>
+  <v-footer color="teal" app bottom absolute style="width: 100%" padless>
+    <v-row class="d-flex justify-space-between">
+      <v-col cols="2" align="center">
+        <p>Fale Conosco</p>
+        <router-link to="/email">
+          <img src="@/assets/images/o-email.png" width="30" height="30" />
+        </router-link>
+        <router-link to="/whatsapp">
+          <img src="@/assets/images/whatsapp.png" width="30" height="30" />
+        </router-link>
       </v-col>
 
+      <v-row class="d-flex align-center">
+        <v-col cols="8" align="center">
+          <img src="@/assets/images/c.png" width="20" height="20" />
+          <span>2022 T40 Bra, Inc. All Rights reserved</span>
+        </v-col>
+      </v-row>
+
       <v-col cols="2" justify="end">
-          <p>Redes Sociais</p>
-          <router-link to="/instagram"
-            ><v-btn>
-              <img
-                src="@/assets/images/instagram.png"
-                width="30"
-                height="30" /></v-btn
-          ></router-link>
-          <router-link to="/facebook"
-            ><v-btn>
-              <img
-                src="@/assets/images/facebook.png"
-                width="30"
-                height="30" /></v-btn
-          ></router-link>
+        <p>Redes Sociais</p>
+        <router-link to="/instagram">
+          <img src="@/assets/images/instagram.png" width="30" height="30" />
+        </router-link>
+        <router-link to="/facebook">
+          <img src="@/assets/images/facebook.png" width="30" height="30" />
+        </router-link>
       </v-col>
     </v-row>
-  </v-toolbar>
+  </v-footer>
 </template>
