75b5efef94bca13dc560287e6a9a2f436897deec
Thiesa <thiesa.c@gmail.com>
2022-11-28 13:08:39 -0400

    fix:admin and footer

--- src/components/Admin/Admin.vue
+++ src/components/Admin/Admin.vue
@@ -36,10 +36,39 @@ export default {
 </script>
 
 <template>
-  <div>
+  <v-layout>
+    <v-app-bar color="primary">
+      <v-app-bar-title>Administrador</v-app-bar-title>
+    </v-app-bar>
+    <v-navigation-drawer color="secondary" permanent>
+      <v-list-nav>
+        <v-list-item
+          @click="handleClickOpenModal()"
+          prepend-icon="mdi-hanger"
+          title="Cadastrar Peça"
+        ></v-list-item>
+        <CreatePecaModal
+          :openModal="openModal"
+          @closeModal="openModal = $event"
+        />
+        <v-list-item
+          prepend-icon="mdi-account-plus"
+          title="Cadastrar Usuário"
+        ></v-list-item>
+        <v-list-item
+          prepend-icon="mdi-cog"
+          title="Configurações Conta"
+        ></v-list-item>
+      </v-list-nav>
+    </v-navigation-drawer>
+    <v-main>
+      <v-card height="400px"></v-card>
+    </v-main>
+  </v-layout>
+  <!-- <div>
     <v-btn variant="flat" color="error" @click="handleClickOpenModal()">
       Cadastrar Peça
     </v-btn>
     <CreatePecaModal :openModal="openModal" @closeModal="openModal = $event" />
-  </div>
+  </div> -->
 </template>
--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -7,11 +7,15 @@ export default {};
     <v-row class="d-flex justify-space-between">
       <v-col cols="2" align="center">
         <p>Fale Conosco</p>
-        <router-link to="/email">
-          <img src="@/assets/images/o-email.png" width="30" height="30" />
+        <router-link to="/email" style="text-decoration: none">
+          <v-btn variant="plain"
+            ><v-icon icon="mdi-email" size="x-large"></v-icon
+          ></v-btn>
         </router-link>
-        <router-link to="/whatsapp">
-          <img src="@/assets/images/whatsapp.png" width="30" height="30" />
+        <router-link to="/whatsapp" style="text-decoration: none">
+          <v-btn variant="plain"
+            ><v-icon icon="mdi-whatsapp" size="x-large"></v-icon
+          ></v-btn>
         </router-link>
       </v-col>
 
@@ -24,11 +28,15 @@ export default {};
 
       <v-col cols="2" justify="end">
         <p>Redes Sociais</p>
-        <router-link to="/instagram">
-          <img src="@/assets/images/instagram.png" width="30" height="30" />
+        <router-link to="/instagram" style="text-decoration: none">
+          <v-btn variant="plain"
+            ><v-icon icon="mdi-instagram" size="x-large"></v-icon
+          ></v-btn>
         </router-link>
-        <router-link to="/facebook">
-          <img src="@/assets/images/facebook.png" width="30" height="30" />
+        <router-link to="/facebook" style="text-decoration: none">
+          <v-btn variant="plain"
+            ><v-icon icon="mdi-facebook" size="x-large"></v-icon
+          ></v-btn>
         </router-link>
       </v-col>
     </v-row>
