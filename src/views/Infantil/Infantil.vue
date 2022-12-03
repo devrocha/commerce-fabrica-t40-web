43d035bc184d721769877d1609e6b97cf9704847
Thiesa <thiesa.c@gmail.com>
2022-12-03 09:28:44 -0400

    chore:remove folders

--- src/views/Infantil/Infantil.vue
+++
@@ -1,7 +0,0 @@
-<script>
-export default {};
-</script>
-
-<template>
-	<h1>Teste Infantil</h1>
-</template>
--- src/views/Outlet/Outlet.vue
+++
@@ -1,7 +0,0 @@
-<script>
-export default {};
-</script>
-
-<template>
-	<h1>Teste Outlet</h1>
-</template>
